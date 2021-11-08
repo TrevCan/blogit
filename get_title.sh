c5fb3124d7458000e458fcf48ae3f2229a4c2b7a
Hector Canizales <heror0484@protonmail.com>
2021-11-08 14:48:32 -0600

    add script description & sources

--- get_title.sh
+++ get_title.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+# script that retrieves the content inside the first
+# set of <title> </title> tags
+# inspiration:
+# https://stackoverflow.com/questions/39650612/how-to-extract-text-between-html-tags-with-or-condition-multiple-times#39651955
+
 
 URL=$(curl -L "$1" | tr -d '\n' |  
 	grep -oz '<title.*title>\|^--.*' |
